19b7e04edd14faa0fe945e03bd4627c3bb9f7d37
TanmeetKaur <84788482+TanmeetKaur@users.noreply.github.com>
2021-06-27 16:22:09 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Java Script and React.js</h1>
+      <p>bootcamp on basic web dev</p>
+    </div>
+  );
+}
+
+export default Info;
---
+++ index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./components/App";
+import Header from "./components/Header";
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+);
